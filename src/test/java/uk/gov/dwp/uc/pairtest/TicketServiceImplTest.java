0b3e68c0445683cc5cfd409d0205c201011b6cfc
Durga-06 <durgaprasad.thoutam@gmail.com>
2026-07-11 23:06:35 +0100

    refactor: extract mock setup into @BeforeEach

--- src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -1,36 +1,43 @@
 package uk.gov.dwp.uc.pairtest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 
 class TicketServiceImplTest {
 
+    private TicketPaymentService ticketPaymentService;
+    private SeatReservationService seatReservationService;
+    private TicketServiceImpl ticketService;
+
+    @BeforeEach
+    void setUp() {
+        ticketPaymentService = mock(TicketPaymentService.class);
+        seatReservationService = mock(SeatReservationService.class);
+        ticketService = new TicketServiceImpl(ticketPaymentService, seatReservationService);
+    }
+
     @Test
     void ticketPurchaseWithNullAccountIdIsRejected() {
-        TicketServiceImpl ticketService = new TicketServiceImpl();
         TicketTypeRequest singleAdult = new TicketTypeRequest(Type.ADULT, 1);
 
-        assertThrows(
-            InvalidPurchaseException.class,
-            () -> ticketService.purchaseTickets(null, singleAdult)
-        );
+        assertThrows(InvalidPurchaseException.class, () -> ticketService.purchaseTickets(null, singleAdult));
     }
 
     @ParameterizedTest
     @ValueSource(longs = {0L, -1L, -999L})
     void ticketPurchaseWithNonPositiveAccountIdIsRejected(long accountId) {
-        TicketServiceImpl ticketService = new TicketServiceImpl();
         TicketTypeRequest singleAdult = new TicketTypeRequest(Type.ADULT, 1);
 
-        assertThrows(
-            InvalidPurchaseException.class,
-            () -> ticketService.purchaseTickets(accountId, singleAdult)
-        );
+        assertThrows(InvalidPurchaseException.class, () -> ticketService.purchaseTickets(accountId, singleAdult));
     }
 }
